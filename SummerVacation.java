b19597f67ef48fe879b53bbb3a8a050a6f067af6
gl-aarav <gl.aarav@gmail.com>
2024-09-07 13:45:27 -0700

    Create SummerVacation.java

---
+++ SummerVacation.java
@@ -0,0 +1,20 @@
+//Aarav Goyal
+//August 28, 2024 Period: 6
+//All About My Summer Break!
+//SummerVacation.java
+
+
+public class SummerVacation
+{
+	public static void main (String[] Args)
+	{
+	
+	System.out.println("\n\n\n\"My Summer Vacation\":");
+	System.out.println("This summer I went to India and Singapore. While there I:");
+	System.out.println("\tVisited family in India\n" +  
+					   "\tVisited Singapore\n" +
+					   "\tSpent time with Family");
+	System.out.println("It was an amazing summer!\n\n\n");
+	}
+}
+
